6895585bf932d2a5448d61d5debdbbb1d971dae5
Marnanel Thurman <marnanel@thurman.org.uk>
2021-11-30 21:01:30 +0000

    onclick code for the login page so logins work

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
         console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-
-        grok_login1(handle_login1);
-
+        $("#submitlogin").click(function() {
+                console.log("login: part 1: here we go!");
+                grok_login1(handle_login1);
+        });
 }
 
 function handle_login1(login1) {
+        console.log("login: part 2");
         grok_login2(
                 handle_login2,
                 login1['auth'],
@@ -22,5 +24,6 @@ function handle_login1(login1) {
 }
 
 function handle_login2(login2) {
+        console.log("login: done");
         console.log(login2);
 }
